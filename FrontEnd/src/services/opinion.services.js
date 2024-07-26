15aef0a2f22726956850f897e1c6cfef69fbca5f
JesusMorales19 <138167542+JesusMorales19@users.noreply.github.com>
2024-07-26 04:14:49 -0600

    Update opinion.services.js

--- FrontEnd/src/services/opinion.services.js
+++ FrontEnd/src/services/opinion.services.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL_API = "http://localhost:3000/api";
+const URL_API = "https://quicklib-ugh1.onrender.com/api";
 
 export const registerOpinion = async (opinion) => {
     return axios.post(`${URL_API}/register/opinion`, opinion);
