7a26874b8ef2ea2a0175d61a5fcc133bc2193555
JesusMorales19 <138167542+JesusMorales19@users.noreply.github.com>
2024-07-26 04:13:58 -0600

    Update consult.services.js

--- FrontEnd/src/services/consult.services.js
+++ FrontEnd/src/services/consult.services.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const URL_API = "http://localhost:3000/api";
+const URL_API = "https://quicklib-ugh1.onrender.com/api";
 
 export const registerConsult = async (consult) => {
   return axios.post(`${URL_API}/register/consult`, consult);
