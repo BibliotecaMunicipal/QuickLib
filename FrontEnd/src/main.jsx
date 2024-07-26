7383d2404041300bd43ad4102e8ee88db3d4fd0a
BibliotecaMunicipal <bibliotecamunicipaljaz@gmail.com>
2024-07-26 04:36:21 -0600

    Update main.jsx

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { NextUIProvider } from "@nextui-org/react";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <NextUIProvider>
     <App />
     <ToastContainer position="top-center" autoClose={3000} />
-  </React.StrictMode>,
-)
+  </NextUIProvider>
+);
   
