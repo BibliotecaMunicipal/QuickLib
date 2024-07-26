5d72e42eef7340badeeffcc54ba2f7b1a60168b9
JesusMorales19 <138167542+JesusMorales19@users.noreply.github.com>
2024-07-26 13:36:24 -0600

    Update loan.services.js

--- FrontEnd/src/services/loan.services.js
+++ FrontEnd/src/services/loan.services.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const URL_API = "https://quicklib-ugh1.onrender.com/api";
+const URL_API = "https://quicklib-backend.onrender.com/api";
 
 export const registerLoans = async (loan, token) => {
   return axios.post(`${URL_API}/register/loan`, loan, {
