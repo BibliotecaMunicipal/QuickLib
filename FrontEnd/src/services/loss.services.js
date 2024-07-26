164c42b11cf05beb3416101d7160767d76dd2140
JesusMorales19 <138167542+JesusMorales19@users.noreply.github.com>
2024-07-26 04:14:38 -0600

    Update loss.services.js

--- FrontEnd/src/services/loss.services.js
+++ FrontEnd/src/services/loss.services.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL_API = "http://localhost:3000/api";
+const URL_API = "https://quicklib-ugh1.onrender.com/api";
 
 export const getLoss = async (token) => {
     return axios.get(`${URL_API}/getAllBook/loss`, { headers: { Authorization: token }})
