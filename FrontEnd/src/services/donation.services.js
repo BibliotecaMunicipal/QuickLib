c0b0eb079c9207f5c333dae0ced5d65b45ebdb73
JesusMorales19 <138167542+JesusMorales19@users.noreply.github.com>
2024-07-26 04:14:11 -0600

    Update donation.services.js

--- FrontEnd/src/services/donation.services.js
+++ FrontEnd/src/services/donation.services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const URL_API = "http://localhost:3000/api";
+const URL_API = "https://quicklib-ugh1.onrender.com/api";
 
 export const getDonations = async (token) => {
     return axios.get(`${URL_API}/getAllBook/donation`, { headers: { Authorization: token}})
